06ee7a5f49a40d02eaf36ec3069cb4d482b41e23
Jan <jan.bogoryja@gmail.com>
2020-08-29 16:38:12 +0200

    v0.0.1 Initial commit
    Add initial Springboot project files
    Add a gitignore

---
+++ src/main/java/dev/hotdeals/treecreate/TreecreateApplication.java
@@ -0,0 +1,15 @@
+package dev.hotdeals.treecreate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreecreateApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TreecreateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/hotdeals/treecreate/TreecreateApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.hotdeals.treecreate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreecreateApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
