7560c5d07111d823d5912314ac498a19145c6ef0
Cristian <cristi.valentin29@gmail.com>
2020-09-27 17:44:25 +0200

    Add Ability to Create Box on Click, Box Amount Limit, Delete Button
    Add the ability to create new boxes in a predetermined location by clicking on the screen
    Add delete button for each box
    Add limit ( max: 25 ) for the amount of boxes that can be created
    Add icon for delete button

--- src/main/resources/static/js/familyTree.js
+++ src/main/resources/static/js/familyTree.js
@@ -1,5 +1,14 @@
+function deleteBox(button)
+{
+    button.parentNode.parentNode.removeChild(button.parentNode);
+}
+
 window.onload = function ()
 {
+    var boundaries = document.getElementById("productContainer");
+    var spawnzone = document.getElementById("draggableBoxSpawnzone");
+    var boxes = document.getElementsByClassName("draggableBox");
+
     var container = document.querySelector("#draggableBoxContainer");
     var activeItem = null;
 
@@ -13,9 +22,38 @@ window.onload = function ()
     container.addEventListener("mouseup", dragEnd, false);
     container.addEventListener("mousemove", drag, false);
 
+    boundaries.addEventListener("click", createBox);
+
+    function createBox(e) {
+        // limit has to be increased by 1, as the very first box is invisible and cannot be interacted with
+        if (boxes.length <= 26)
+        {
+            if (e.target === boundaries)
+            {
+                clone = boxes[0].cloneNode(true)
+
+                clone.style.top = 50 + '%';
+                clone.style.left = 50 + '%';
+                clone.style.display = 'flex';
+                clone.style.position = 'absolute';
+                clone.style.height = 250 + 'px';
+                clone.style.width = 500 + 'px';
+
+                spawnzone.appendChild(clone);
+
+            }
+        }
+        else
+        {
+            alert("Cannot exceed limit of 25 boxes.");
+        }
+
+    }
+
     function dragStart(e) {
 
-        if (e.target !== e.currentTarget) {
+        if (e.target !== e.currentTarget)
+        {
             active = true;
 
             // this is the item we are interacting with
@@ -34,7 +72,6 @@ window.onload = function ()
                     activeItem.initialX = e.touches[0].clientX - activeItem.xOffset;
                     activeItem.initialY = e.touches[0].clientY - activeItem.yOffset;
                 } else {
-                    console.log("doing something!");
                     activeItem.initialX = e.clientX - activeItem.xOffset;
                     activeItem.initialY = e.clientY - activeItem.yOffset;
                 }
