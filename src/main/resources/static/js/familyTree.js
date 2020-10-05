18fde7052c4142f7818777a3a9f6bf756d0aca7b
Cristian <cristi.valentin29@gmail.com>
2020-10-05 14:06:36 +0200

    Refactor Code
    Boom boom ciao

--- src/main/resources/static/js/familyTree.js
+++ src/main/resources/static/js/familyTree.js
@@ -1,3 +1,4 @@
+// triggered by deleteDraggableBoxButton
 function deleteBox(button)
 {
     button.parentNode.parentNode.removeChild(button.parentNode);
@@ -6,53 +7,51 @@ function deleteBox(button)
 window.onload = function ()
 {
     var boundaries = document.getElementById("draggableBoxContainer");
-    var spawnzone = document.getElementById("draggableBoxContainer");
     var boxes = document.getElementsByClassName("draggableBox");
-
-    var container = document.querySelector("#draggableBoxContainer");
     var activeItem = null;
-
     var active = false;
 
-    boundaries.addEventListener("click", createBox);
+    boundaries.addEventListener("mousedown", createBox);
+    boundaries.addEventListener("mousemove", drag, false);
 
+    // creates a box on the cursor coordinates
     function createBox(e)
     {
         if (e.target === boundaries)
         {
-            // limit has to be increased by 1, as the very first box is invisible and cannot be interacted with
+            // limit has to be increased by 1, as the very first box is invisible and
+            // cannot be interacted with
             if (boxes.length <= 26)
             {
-                clone = boxes[0].cloneNode(true)
+                clone = boxes[0].cloneNode(true);
 
                 var boxWidth = 500;
                 var boxHeight = 250;
+
+                // box style
+                clone.style.display = 'flex';
+                clone.style.position = 'absolute';
+                clone.style.width = boxWidth + 'px';
+                clone.style.height = boxHeight + 'px';
 
                 var cursorX = e.clientX;
                 var cursorY = e.clientY;
                 var offsetX = boxWidth / 2;
                 var offsetY = boxHeight / 2;
 
-                clone.style.display = 'flex';
-                clone.style.position = 'absolute';
-                clone.style.width = boxWidth + 'px';
-                clone.style.height = boxHeight + 'px';
-
-                spawnzone.appendChild(clone);
+                // create the box at the cursor coordinates
+                boundaries.appendChild(clone);
                 clone.style.top = cursorY - offsetY + 'px';
                 clone.style.left = cursorX - offsetX + 'px';
 
+                // add events listeners on the drag button
                 var button = clone.getElementsByClassName("dragDraggableBoxButton")[0];
-
-                console.log(button.className);
+                // touch input
                 button.addEventListener("touchstart", dragStart, false);
                 button.addEventListener("touchend", dragEnd, false);
-                button.addEventListener("touchmove", drag, false);
-
+                // mouse input
                 button.addEventListener("mousedown", dragStart, false);
                 button.addEventListener("mouseup", dragEnd, false);
-                button.addEventListener("mousemove", drag, false);
-
             }
             else
             {
@@ -61,37 +60,42 @@ window.onload = function ()
         }
     }
 
-    function dragStart(e) {
-        //if (e.target !== e.currentTarget)
-        //{
-            active = true;
+    function dragStart(e)
+    {
+        active = true;
 
-            // this is the item we are interacting with
-            activeItem = e.target.parentNode;
-            console.log(activeItem.className);
+        // this is the item we are interacting with
+        activeItem = e.target.parentNode;
 
-            if (activeItem !== null) {
-                if (!activeItem.xOffset) {
-                    activeItem.xOffset = 0;
-                }
-
-                if (!activeItem.yOffset) {
-                    activeItem.yOffset = 0;
-                }
-
-                if (e.type === "touchstart") {
-                    activeItem.initialX = e.touches[0].clientX - activeItem.xOffset;
-                    activeItem.initialY = e.touches[0].clientY - activeItem.yOffset;
-                } else {
-                    activeItem.initialX = e.clientX - activeItem.xOffset;
-                    activeItem.initialY = e.clientY - activeItem.yOffset;
-                }
+        if (activeItem !== null)
+        {
+            if (!activeItem.xOffset)
+            {
+                activeItem.xOffset = 0;
             }
-        //}
+
+            if (!activeItem.yOffset)
+            {
+                activeItem.yOffset = 0;
+            }
+
+            if (e.type === "touchstart")
+            {
+                activeItem.initialX = e.touches[0].clientX - activeItem.xOffset;
+                activeItem.initialY = e.touches[0].clientY - activeItem.yOffset;
+            }
+            else
+            {
+                activeItem.initialX = e.clientX - activeItem.xOffset;
+                activeItem.initialY = e.clientY - activeItem.yOffset;
+            }
+        }
     }
 
-    function dragEnd(e) {
-        if (activeItem !== null) {
+    function dragEnd(e)
+    {
+        if (activeItem !== null)
+        {
             activeItem.initialX = activeItem.currentX;
             activeItem.initialY = activeItem.currentY;
         }
@@ -100,14 +104,19 @@ window.onload = function ()
         activeItem = null;
     }
 
-    function drag(e) {
-        if (active) {
-            if (e.type === "touchmove") {
+    function drag(e)
+    {
+        if (active)
+        {
+            if (e.type === "touchmove")
+            {
                 e.preventDefault();
 
                 activeItem.currentX = e.touches[0].clientX - activeItem.initialX;
                 activeItem.currentY = e.touches[0].clientY - activeItem.initialY;
-            } else {
+            }
+            else
+            {
                 activeItem.currentX = e.clientX - activeItem.initialX;
                 activeItem.currentY = e.clientY - activeItem.initialY;
             }
@@ -119,7 +128,8 @@ window.onload = function ()
         }
     }
 
-    function setTranslate(xPos, yPos, el) {
-        el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+    function setTranslate(xPos, yPos, element)
+    {
+        element.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
     }
 }
