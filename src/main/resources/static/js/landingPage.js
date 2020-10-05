878b38feddcdee84be7c9a934379ceb7cf946c03
Cristian <cristi.valentin29@gmail.com>
2020-10-05 16:16:34 +0200

    Add newsletter confirmation popup

---
+++ src/main/resources/static/js/landingPage.js
@@ -0,0 +1,6 @@
+function submitPopup() {
+    var popup = document.getElementById("landingPagePopup");
+    //popup.style.display='inline-block';
+    //popup.style.top = window.screen.height * 0.1 + 'px';
+    popup.classList.toggle("show");
+}
