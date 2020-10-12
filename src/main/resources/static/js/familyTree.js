cdc6c3b192229e1474c2eb51e6f626a414e81b66
Cristian <cristi.valentin29@gmail.com>
2020-10-12 12:56:55 +0200

    v.o.5.9 Add Draggable Boxes Finishing
    Fixed the creation of boxes for new size
    Fixed the size of the input & buttons for the boxes
    Replace the buttons with new design

--- src/main/resources/static/js/familyTree.js
+++ src/main/resources/static/js/familyTree.js
@@ -1,3 +1,10 @@
+function viewportToPixels(value) {
+    var parts = value.match(/([0-9\.]+)(vh|vw)/)
+    var q = Number(parts[1])
+    var side = window[['innerHeight', 'innerWidth'][['vh', 'vw'].indexOf(parts[2])]]
+    return side * (q/100)
+}
+
 function getRandomInt(min, max)
 {
     min = Math.ceil(min);
@@ -50,25 +57,27 @@ window.onload = function () {
             {
                 let clone = boxes[0].cloneNode(true);
 
-                const boxWidth = 500;
-                const boxHeight = 250;
+                const boxWidth = 10;
+                const boxHeight = 10;
 
                 // box style
                 clone.style.display = 'flex';
                 clone.style.flexWrap = 'wrap';
                 clone.style.position = 'absolute';
-                clone.style.width = boxWidth + 'px';
-                clone.style.height = boxHeight + 'px';
+                clone.style.width = boxWidth + 'vw';
+                clone.style.height = boxHeight + 'vh';
 
                 let cursorX = e.clientX;
                 let cursorY = e.clientY;
-                let offsetX = boxWidth / 2;
-                let offsetY = boxHeight / 2;
+                let parentX = boundaries.offsetLeft;
+                let parentY = boundaries.offsetTop;
+                let offsetX = viewportToPixels(boxWidth + 'vw') / 2;
+                let offsetY = viewportToPixels(boxHeight + 'vh') / 2;
 
                 // create the box at the cursor coordinates
                 boundaries.appendChild(clone);
-                clone.style.top = cursorY - offsetY + 'px';
-                clone.style.left = cursorX - offsetX + 'px';
+                clone.style.left = cursorX - parentX - offsetX + 'px';
+                clone.style.top = cursorY - parentY - offsetY + 'px';
                 clone.style.background = assignBoxBackground(images);
                 clone.style.backgroundSize = '100% 100%';
 
