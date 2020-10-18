43cb835afb57cd04d824a70143285ba37613c595
Jan <jan.bogoryja@gmail.com>
2020-10-18 21:57:40 +0200

    v0.7.6 Fix invalid payment test value
    If you want to test you mark test as true and add test to the hash...not just write true

--- src/main/resources/static/js/payment.js
+++ src/main/resources/static/js/payment.js
@@ -5,7 +5,17 @@ function updateHash()
     const customerRef = document.getElementById("customer_ref").value.toString();
     const amount = document.getElementById("amount").value.toString();
     const currency = document.getElementById("currency").value.toString();
-    const test = document.getElementById("test").value.toString();
+    let test = document.getElementById("test").value.toString();
+
+    // Because fuck my brain, if you want to test you mark test as true and add test to the hash...
+    // Thanks nordea, I've spent an hour on this
+    if (test === "true")
+    {
+        test = "test";
+    } else
+    {
+        test = "";
+    }
 
     let combinedString = merchantId + paymentRef + customerRef + amount + currency + test;
     console.log("Combined string: " + combinedString)
