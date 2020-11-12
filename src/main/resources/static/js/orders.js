179bed91b239450610c056964d3c3abca3ce49e8
Jan <jan.bogoryja@gmail.com>
2020-11-12 04:44:04 +0100

    v0.9.29 Add order designs to the orders menu
    Before it wouldn't show the designs (actually orders) in the order (actually transaction/payment)

--- src/main/resources/static/js/orders.js
+++ src/main/resources/static/js/orders.js
@@ -1,7 +1,14 @@
+const orderSizes = new Map([
+    ["20x20 cm", 495],
+    ["25x25 cm", 695],
+    ["30x30 cm", 995]
+]);
+
 async function generateOrders()
 {
     console.log("%cHey orders viewer, thanks for summoning me, ze order generator", "color:mediumpurple");
     const transactionBaseline = document.getElementsByClassName("orderContainer")[0];
+    const orderBaseline = document.getElementsByClassName("orderDesignContainer")[0];
     const detailsPage = document.getElementById("details");
     const transactionsJson = await fetchTransactions();
 
@@ -11,13 +18,11 @@ async function generateOrders()
         return;
     }
     let transactions = JSON.parse(transactionsJson);
-    //console.log("Order: " + transactionsJson)
     console.log("transaction count: " + (transactions.length + 1))
     for (let i = 0; i < transactions.length; i++)
     {
         let transaction = transactions[i];
         console.log("Processing transaction : " + i)
-        //console.log(transaction)
         let transactionItem = transactionBaseline.cloneNode(true);
         transactionItem.style.display = "flex";
 
@@ -32,7 +37,7 @@ async function generateOrders()
         }
 
         transactionItem.getElementsByClassName('orderStatus')[0].innerText = "Status: " + status;
-        transactionItem.getElementsByClassName('orderTotalPrice')[0].innerText = "Total Price: " + (transaction['price']/100) + " dkk";
+        transactionItem.getElementsByClassName('orderTotalPrice')[0].innerText = "Total Price: " + (transaction['price'] / 100) + " dkk";
         console.log("transaction id: " + transaction['id']);
         transactionItem.getElementsByClassName("orderTransactionId")[0].innerText = "transaction ID: " + transaction['id'];
 
@@ -48,12 +53,43 @@ async function generateOrders()
         }
         transactionItem.getElementsByClassName("orderDateCreated")[0].innerText = "Created on: " + createdOn;
         transactionItem.getElementsByClassName("orderExpectedDelivery")[0].innerText = "Expected Delivery Date: " + transaction['expectedDeliveryDate'];
-
         transactionItem.getElementsByClassName("orderAddressStreetName")[0].innerText = "Street: " + transaction['streetAddress'];
         transactionItem.getElementsByClassName("orderAddressCity")[0].innerText = "City: " + transaction['city'];
         transactionItem.getElementsByClassName("orderAddressPostcode")[0].innerText = "Postcode: " + transaction['postcode'];
 
+        //console.log("Transaction object:")
+        //console.dir(transaction);
+        const orderList = transaction['orderList'];
+        //console.dir(orderList)
+        for (let i = 0; i < orderList.length; i++)
+        {
+            const order = orderList[i];
+            console.log("Processing order " + order['orderId']);
+            let orderItem = orderBaseline.cloneNode(true);
+            orderItem.style.display = "flex";
+
+            const design = order['treeDesignById'];
+            const designJson = JSON.parse(design['designJson'])
+
+            let itemPrice = orderSizes.get(order.size);
+            let amount = parseInt(order.amount);
+            let itemTotal = (itemPrice * amount);
+            let itemSaved = 0;
+            if (amount > 3)
+            {
+                itemSaved = itemTotal * 0.25;
+                itemTotal *= 0.75;
+            }
+            orderItem.getElementsByClassName("orderDesignName")[0].innerText = designJson.name;
+            orderItem.getElementsByClassName("orderDesignSize")[0].innerText = order.size;
+            orderItem.getElementsByClassName("orderDesignAmount")[0].innerText = 'amount:' + order.amount;
+            orderItem.getElementsByClassName("orderDesignPrice")[0].innerText = 'price: ' + itemTotal + 'kr';
+
+            transactionItem.getElementsByClassName("orderDesignInfo")[0].append(orderItem);
+        }
+
         detailsPage.append(transactionItem)
+        console.log("Finished generating transaction id: " + i)
     }
 }
 
@@ -63,3 +99,9 @@ async function fetchTransactions()
     console.log("%cFetching transactions has finished, status: " + response.status, "color:mediumpurple");
     return await response.text();
 }
+
+function viewDesign(e)
+{
+    console.log("Viewing X");
+}
+
