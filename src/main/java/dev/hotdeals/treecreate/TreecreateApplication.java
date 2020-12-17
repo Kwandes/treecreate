e942078181cf10621e818980e7d4e02b27c71c28
Jan <jan.bogoryja@gmail.com>
2020-12-17 01:16:23 +0100

    v1.1.13 Convert to a war file, Add Tomcat support
    Also, Bump version

--- src/main/java/dev/hotdeals/treecreate/TreecreateApplication.java
+++ src/main/java/dev/hotdeals/treecreate/TreecreateApplication.java
@@ -2,9 +2,10 @@ package dev.hotdeals.treecreate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TreecreateApplication
+public class TreecreateApplication extends SpringBootServletInitializer
 {
 
     public static void main(String[] args)
