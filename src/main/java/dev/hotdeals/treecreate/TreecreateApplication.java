b280634e51854c034ed8ce99ff104b7435980e39
Alex <nyt604@gmail.com>
2020-12-18 17:53:05 +0100

    Tutorial changes hopefully it works
    you know it baby

--- src/main/java/dev/hotdeals/treecreate/TreecreateApplication.java
+++ src/main/java/dev/hotdeals/treecreate/TreecreateApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TreecreateApplication extends SpringBootServletInitializer
+public class TreecreateApplication
 {
 
     public static void main(String[] args)
